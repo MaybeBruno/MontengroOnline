e1923b22b7467e8a41dc456a2db91c37c89baa68
Bruno Vega <bruno.vega.marquez@gmail.com>
2025-06-24 19:44:33 -0400

    Creacion paguina Home con su ruta

---
+++ Front-End-React/front-end-react/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-3xl font-bold text-blue-600">Bienvenido a Montenegro Online</h1>
+      <p className="mt-4 text-gray-600">Este será tu panel principal para comenzar a construir 🍕</p>
+    </div>
+  );
+};
+
+export default Home;
