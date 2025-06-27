23ea946962c36c4f844128e4d8ecbdc5c7fcf467
Bruno Vega <bruno.vega.marquez@gmail.com>
2025-06-26 21:38:59 -0400

    Pruba

--- Front-End-React/front-end-react/src/pages/Home.jsx
+++ Front-End-React/front-end-react/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="p-8 text-center">
-      <h1 className="text-3xl font-bold text-blue-600">Bienvenido a Montenegro Online</h1>
+      <h1 className="text-3xl font-bold text-blue-600">Hola a Montenegro Online</h1>
       <p className="mt-4 text-gray-600">Este será tu panel principal para comenzar a construir 🍕</p>
     </div>
   );
