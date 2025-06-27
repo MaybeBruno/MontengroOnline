5c7e8c3992385090c13b18be4ee2c652e39459e7
Benjamin Sepulveda <ziplocsepulvedagodoy0405@gmail.com>
2025-06-26 22:17:26 -0400

    modify: Logo header

--- Front-End-React/front-end-react/src/components/header.jsx
+++ Front-End-React/front-end-react/src/components/header.jsx
@@ -1,10 +1,18 @@
 import './header.css';
 import { NavLink } from 'react-router-dom';
-
+import logonegro from '../assets/logo-fondo-nrego.webp';
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="logo">Montenegro's</h1>
+     <div className="Container-logo">
+               <img
+                 src={logonegro}
+                 alt="Logo header"
+                 className="logo-imagen"
+               />
+               <h1 className="logo">Montenegro's</h1>
+             </div>
+      
       <nav className="nav">
         <NavLink to="/"    className="nav-link">Inicio</NavLink>
         <NavLink to="/register" className="nav-link">Registro</NavLink>
